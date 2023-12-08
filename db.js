75a20d47354c1365f5c7978dda03493524200d79
Priyanshu Raj <priyanshur754@gmail.com>
2023-12-09 00:33:06 +0500

    Added async await to function

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
 const mongoURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@inotebookdb.lul0odf.mongodb.net/`
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI, {
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI, {
     }).then(() => {
         console.log("Connection is successfull :)");
     }).catch((e) => {
-        console.log("NOT Connection :( => " + e);
+        console.log("NOT Connected :( Error: " + e);
     });
 }
 
