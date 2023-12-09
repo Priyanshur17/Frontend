cbfc3741b27de0af362aed9a71417ea60bdeed07
Priyanshu Raj <priyanshur754@gmail.com>
2023-12-09 19:56:44 +0500

    commit desc Added Routes folder Made Schema for user & notes Added models for both user & notes

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const port = 8000;
 
 app.get("/", (req, res) => res.status(200).send("Hello Me Here"))
 
+// Availabe Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
